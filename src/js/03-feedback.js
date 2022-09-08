3aba6c429d7c1964a7ed7bac389d66f33aa22a72
uTatsii <utatsiy@gmail.com>
2022-09-08 18:50:36 +0300

    almost done task-03

--- src/js/03-feedback.js
+++ src/js/03-feedback.js
@@ -1,1 +1,40 @@
+import throttle from 'lodash.throttle';
 
+const SAVED_FORM_DATA_KEY = 'feedback-form-state';
+
+const form = document.querySelector('.feedback-form');
+const input = document.querySelector('input');
+const textarea = document.querySelector('textarea');
+const savedData = {
+  email: '',
+  message: '',
+};
+
+form.addEventListener('submit', onFormSubmit);
+form.addEventListener('input', throttle(onFormInput, 500));
+window.addEventListener('load', formUpdate);
+
+function onFormSubmit(e) {
+  e.preventDefault();
+
+  console.log(savedData);
+
+  localStorage.removeItem(SAVED_FORM_DATA_KEY);
+
+  form.reset();
+}
+
+function onFormInput() {
+  savedData.email = input.value;
+  savedData.message = textarea.value;
+
+  localStorage.setItem(SAVED_FORM_DATA_KEY, JSON.stringify(savedData));
+}
+
+function formUpdate() {
+  const localFormData = JSON.parse(localStorage.getItem(SAVED_FORM_DATA_KEY));
+    if (localFormData) {
+        input.value = localFormData.email;
+        textarea.value = localFormData.message;
+  }
+}
