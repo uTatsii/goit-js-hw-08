68bd70e1eb273e5252c8dfd5184253f483ab717b
uTatsii <utatsiy@gmail.com>
2022-09-08 16:51:28 +0300

    task-02

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,1 +1,23 @@
+import VimeoPlayer from '@vimeo/player';
+import throttle from 'lodash.throttle';
+
+const PLAYBACK_POSITION = "videoplayer-current-time";
+
+const iframe = document.querySelector('iframe');
+const player = new VimeoPlayer(iframe);
+const savedTimePosition = localStorage.getItem(PLAYBACK_POSITION);
+const throttle = require('lodash.throttle');
+
+function onVimeoPlayerUpdate({ seconds }) {
+   localStorage.setItem(PLAYBACK_POSITION, seconds);
+}
+
+if (!player) {
+  return;
+} else {
+  savedTimePosition ? player.setCurrentTime(savedTimePosition) : null;
+
+  player.on('timeupdate', throttle(onVimeoPlayerUpdate, 1000));
+}
+
 
